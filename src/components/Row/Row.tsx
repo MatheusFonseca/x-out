e3d6f6e744e5ada43a74da3033d5bdd7acd39409
Matheus Fonseca de Morais <matheus.fonseca@inatel.br>
2022-03-30 23:21:45 -0300

    feat(Row): Create function to mark a number off
    - Create tests with the rules for marking a number off
    - Create a function that implement this rules

--- src/components/Row/Row.tsx
+++ src/components/Row/Row.tsx
@@ -14,6 +14,48 @@ const rowStyles = {
   blue: 'bg-blue',
 };
 
+export interface INumberState {
+  markedOff: boolean;
+  disabled: boolean;
+}
+
+export const markNumber = (
+  target: number,
+  rowState: INumberState[]
+): INumberState[] => {
+  if (rowState[target].disabled || !rowState[target]) {
+    return rowState;
+  }
+
+  const newRowState: INumberState[] = JSON.parse(JSON.stringify(rowState));
+  let markedCount = 0;
+
+  newRowState.forEach((dice, index) => {
+    if (index < target && !dice.markedOff) {
+      dice.disabled = true;
+    }
+    if (dice.markedOff) {
+      markedCount += 1;
+    }
+  });
+
+  // should not mark off the last number if there is less than 5 other marked off numbers before it
+  if (target === rowState.length - 1 && markedCount < 5) {
+    return rowState;
+  }
+
+  newRowState[target].markedOff = true;
+  markedCount += 1;
+
+  // should disable the last number if its not possible to mark 5 numbers before it
+  const numbersLeft = newRowState.length - target - 2;
+  if (markedCount + numbersLeft < 5) {
+    newRowState[newRowState.length - 1].disabled = true;
+  }
+
+  return newRowState;
+};
+
 export const Row = ({ color, inverted = false }: RowProps) => {
   const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
   if (inverted) {
